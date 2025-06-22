31d3d3077c0924aa20ff9fdfb8d9d57e836007d8
Valeriy Lebedev <vqlev.84@yandex.ru>
2025-06-22 20:16:25 +0400

    The final tasks of module 18 have been added. Custom filters and tags have been created.

---
+++ news/templatetags/custom_filters.py
@@ -0,0 +1,45 @@
+from django import template
+
+
+class FilterException(Exception):
+   pass
+
+
+register = template.Library()
+
+
+CURRENCIES_SYMBOLS = {
+   'rub': 'Р',
+   'usd': '$',
+}
+
+
+@register.filter()
+def currency(value, code='rub'):
+   """
+   value: значение, к которому нужно применить фильтр
+   code: код валюты
+   """
+   postfix = CURRENCIES_SYMBOLS[code]
+
+   return f'{value} {postfix}'
+
+
+OBSCENE_WORDS = ['объем', 'развития', "один", "торонто","юниоры"]
+
+
+@register.filter()
+def censor(value):
+   if not  isinstance(value, str):
+      raise FilterException("Фильтр применяется только к строке")
+
+
+   for word in OBSCENE_WORDS:
+       word_low = word.title()
+       value = value.replace(word,word[0]+ '*' * (len(word) - 1))
+       value = value.replace(word_low, word_low[0] + '*' * (len(word) - 1))
+   return value
+
+
+
+
